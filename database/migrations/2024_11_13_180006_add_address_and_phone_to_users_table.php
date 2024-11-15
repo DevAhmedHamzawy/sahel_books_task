7ff69282fb30394464c06e344cd470effba790a5
Ahmed_Hamzawy <ahmedhamzawy3@gmail.com>
2024-11-15 22:59:52 +0200

    updates

--- database/migrations/2024_11_13_180006_add_address_and_phone_to_users_table.php
+++ database/migrations/2024_11_13_180006_add_address_and_phone_to_users_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('address')->after('email');
-            $table->string('phone')->after('email');
+            $table->text('address')->after('email')->nullable();
+            $table->string('phone')->after('email')->nullable();
         });
     }
 
